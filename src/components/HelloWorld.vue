ea06276efaa179ac656c33516a01b18f1248a16b
subject026 <sudobuildspeakers@gmail.com>
2018-09-13 00:30:22 +0100

    new app from vue cli with all the nice bits

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>BOOM</h1>
+    <h1>boom</h1>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -32,11 +34,11 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
